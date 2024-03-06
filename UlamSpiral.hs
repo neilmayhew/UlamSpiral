9ed1e7adf5769ecda93524c08c926e9820f3d0d7
Neil Mayhew <neil@mayhew.name>
2024-03-06 14:30:05 -0700

    Use a colour mapping adapted to the width of the spiral

--- UlamSpiral.hs
+++ UlamSpiral.hs
@@ -21,13 +21,14 @@ main = do
 ulamSpiral :: PreparedFont (N B) -> Int -> Diagram B
 ulamSpiral font w =
   foldMap
-    (ulamCircle font)
+    (ulamCircle font colour)
     [(x, y) | x <- [-r .. r], y <- [-r .. r]]
  where
   r = w `div` 2
+  colour i = uncurryRGB sRGB $ hsv (180 - realToFrac i * 360 / realToFrac (r + 1)) 0.35 0.9
 
-ulamCircle :: PreparedFont (N B) -> (Int, Int) -> Diagram B
-ulamCircle font (x, y) =
+ulamCircle :: PreparedFont (N B) -> (Int -> Colour (N B)) -> (Int, Int) -> Diagram B
+ulamCircle font colour (x, y) =
   let r = max (abs x) (abs y)
       d = 2 * r
       innerArea = (d - 1) ^ (2 :: Int)
@@ -36,11 +37,10 @@ ulamCircle font (x, y) =
         | abs x > abs y = if x > 0 then 0 * d + y else 2 * d - y
         | otherwise     = if y > 0 then 1 * d - x else 3 * d + x
       number = midRightIndex + midRightOffset
-      colour i = uncurryRGB sRGB $ hsv (180 + realToFrac (i - 1) * 36) 0.35 0.9
    in translate (realToFrac <$> x ^& y) $
         mconcat
           [ label font (show number) # fc white
-          , circle 0.5 # lw none # fc (colour (-r))
+          , circle 0.5 # lw none # fc (colour r)
           ]
 
 label :: PreparedFont (N B) -> String -> Diagram B
